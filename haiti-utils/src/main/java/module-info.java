fe43edb9432f47d6bf01f83e85ec4445f488be87
Struchkov Mark <mark@struchkov.dev>
2022-12-17 18:39:28 +0300

    v.2.3.3

--- haiti-utils/src/main/java/module-info.java
+++ haiti-utils/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module haiti.utils {
     exports dev.struchkov.haiti.utils;
+    exports dev.struchkov.haiti.utils.concurrent;
     exports dev.struchkov.haiti.utils.domain;
 
     requires haiti.exception;
